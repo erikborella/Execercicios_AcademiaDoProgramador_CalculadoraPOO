ae759f4a496f1f21ac00d89ba7ad87af360d6e5f
Erik Borella <erikborella@gmail.com>
2021-04-20 17:37:21 -0300

    Adição de tudo

---
+++ CalculadoraPoo/CalculadoraPoo/Calculadora.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace CalculadoraPoo
+{
+    class Calculadora
+    {
+        List<string> historico = new List<string>();
+
+        public double Somar(double n1, double n2) {
+            double resultado = n1 + n2;
+            string conta = $"{n1} + {n2} = {resultado}";
+
+            historico.Add(conta);
+
+            return resultado;
+        }
+
+        public double Subtrair(double n1, double n2)
+        {
+            double resultado = n1 - n2;
+            string conta = $"{n1} - {n2} = {resultado}";
+
+            historico.Add(conta);
+
+            return resultado;
+        }
+
+        public double Multiplicar(double n1, double n2)
+        {
+            double resultado = n1 * n2;
+            string conta = $"{n1} * {n2} = {resultado}";
+
+            historico.Add(conta);
+
+            return resultado;
+        }
+
+        public double Dividir(double n1, double n2)
+        {
+            double resultado = n1 / n2;
+            string conta = $"{n1} / {n2} = {resultado}";
+
+            historico.Add(conta);
+
+            return resultado;
+        }
+
+        public string[] PegarHistorico()
+        {
+            return historico.ToArray();
+        }
+    }
+}
---
+++ CalculadoraPoo/CalculadoraPoo/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace CalculadoraPoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();
+
+            while (true)
+            {
+                int opcao = MenuDeOpcao("1. Somar\n" +
+                    "2. Subtrair\n" +
+                    "3. Multiplicar\n" +
+                    "4. Dividir\n" +
+                    "5. Ver Historico\n" +
+                    "6. Sair\n" +
+                    "Digite o que deseja fazer: ",
+                    "Voce digitou uma operação incorreta, tente novamente",
+                    1, 6);
+
+                if (opcao == 5)
+                {
+                    VerHistorico(calculadora);
+                    continue;
+                }
+
+                if (opcao == 6)
+                {
+                    break;
+                }
+
+                double n1, n2;
+
+                n1 = LerDouble("Primeiro");
+
+                while (true)
+                {
+                    n2 = LerDouble("Segundo");
+
+                    if (opcao == 4 && n2 == 0)
+                    {
+                        Console.WriteLine("Voce não pode dividir por 0");
+                        continue;
+                    }
+                    break;
+                }
+
+                switch (opcao)
+                {
+                    case 1:
+                        calculadora.Somar(n1, n2);
+                        break;
+                    case 2:
+                        calculadora.Subtrair(n1, n2);
+                        break;
+                    case 3:
+                        calculadora.Multiplicar(n1, n2);
+                        break;
+                    case 4:
+                        calculadora.Dividir(n1, n2);
+                        break;
+                }
+                ImprimirUltimaContaConta(calculadora);
+            }
+        }
+
+        private static void VerHistorico(Calculadora calculadora)
+        {
+            Console.Clear();
+            string[] historico = calculadora.PegarHistorico();
+            if (historico.Length == 0)
+            {
+                Console.WriteLine("Voce não tem nenhuma conta feita, faça alguma!");
+            }
+            foreach(string conta in historico)
+            {
+                Console.WriteLine(conta);
+            }
+            Pausar();
+        }
+
+        private static void ImprimirUltimaContaConta(Calculadora calculadora)
+        {
+            Console.Clear();
+            string[] historico = calculadora.PegarHistorico();
+            Console.WriteLine(historico[historico.Length - 1]);
+            Pausar();
+            Console.Clear();
+
+        }
+
+        private static double LerDouble(string ordem)
+        {
+            bool numeroLidoEhNumero = false;
+            double numero = 0.0;
+
+            do
+            {
+                Console.Write($"Digite o {ordem} numero: ");
+                string numeroString = Console.ReadLine();
+                numeroLidoEhNumero = double.TryParse(numeroString, out numero);
+
+                if (!numeroLidoEhNumero)
+                {
+                    Console.WriteLine("Por Favor, Digite um numero!");
+                }
+
+            } while (!numeroLidoEhNumero);
+
+            return numero;
+        }
+
+        private static bool OpcaoEstaCorreta(int opMin, int opMax, int op)
+        {
+            return (op >= opMin && op <= opMax);
+        }
+
+        private static int MenuDeOpcao(string msg, string erroMsg, int opMin, int opMax)
+        {
+            int opcao = opMin - 1;
+
+            while (opcao <= opMin - 1)
+            {
+                Console.Write(msg);
+                opcao = Convert.ToInt32(Console.ReadLine());
+
+                if (!OpcaoEstaCorreta(opMin, opMax, opcao))
+                {
+                    Console.WriteLine(erroMsg);
+                    opcao = opMin - 1;
+                }
+            }
+
+            return opcao;
+        }
+
+        private static void Pausar()
+        {
+            Console.WriteLine("\nDigite qualquer coisa para continuar: ");
+            Console.ReadLine();
+        }
+    }
+}
